4e69381feeb5ab8e89b865211c8257364fefb1fe
Ondra <ondra.baudisch@centrum.cz>
2023-03-04 11:25:06 +0100

    clearing code

--- examples/strings/lib.v
+++ examples/strings/lib.v
@@ -1,10 +1,6 @@
 module test
 
 [export: 'my_fn']
-fn my_fn(a string, b int) string{
-	mut c := a
-	for _ in 0..b{
-		c += a
-	}
-	return c
+pub fn my_fn(a string, b int) string{
+	return a.repeat(b)
 }
